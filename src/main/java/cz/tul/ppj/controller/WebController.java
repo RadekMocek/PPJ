fb9b7eab3ca3dbdc9c9609f395fb9ee2d48a49d9
RadekMocek <radek.mocek@tul.cz>
2025-04-21 21:48:36 +0200

    thymeleaf

---
+++ src/main/java/cz/tul/ppj/controller/WebController.java
@@ -0,0 +1,14 @@
+package cz.tul.ppj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "index";
+    }
+
+}
